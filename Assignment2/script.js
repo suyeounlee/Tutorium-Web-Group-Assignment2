434c6c66331f3eebaa25aff09eb4afc46e6398ff
Suyeon Lee <lsy7674@gmail.com>
2022-11-29 10:24:26 -0500

    Find tutors - select option search

--- Assignment2/script.js
+++ Assignment2/script.js
@@ -1,3 +1,16 @@
+// const button = document.querySelector("#site");
+// const site = document.querySelector(".flexbox-container-siteMap");
+
+// button.addEventListener('click', ()=> {
+//     if(site.style.display === 'none') {
+//         site.style.display = 'flex';
+//         button.value = 'Hide Sitemap';
+//     } else{
+//         site.style.display = 'none';
+//         button.value = 'Show Sitemap'
+//     }
+// });
+
 /*  Suyeon: findTutors.html  */
 
 const searchBox1 = document.getElementById('searchBox');
@@ -24,44 +37,60 @@ function searchName() {
                 tutorBox[i].style.display="none";
             }
         }
+    } //for
+
+
+   
+   
+   
+}; //searchName
+
+
+ 
+const selectId = document.getElementById("findOption").value;
+const optionList = document.querySelectorAll(".option");
+
+
+
+function getSelectValue() {
+
+    let selectedValue = document.getElementById("findOption").value;
+    //console.log(selectedValue);
+    const subject = document.querySelectorAll(".subject");
+    //console.log(subject);
+
+    if(selectedValue === 'subject') {
+        console.log('subject clicked');
+
+        searchBox1.addEventListener("keyup", searchSubject);
+      
+        function searchSubject() {
+            console.log('function searchSubject working?');
+
+            const tutorBox = document.querySelectorAll(".tutorBox");
+            const searchBox = document.getElementById("searchBox").value.toLowerCase();
+
+            for(var i=0; i<subject.length; i++) {
+                let match = tutorBox[i].querySelector('.subject');
+                if(match) {
+                    let textValue = match.textContent || match.innerHTML
+                    
+                    if(textValue.toLowerCase().includes(searchBox)) {
+                        tutorBox[i].style.display="";
+                    } else {
+                        tutorBox[i].style.display="none";
+                    }
+                }
+            } //for
+        }
     }
-}
 
-//SUyeon working with select options
-
-// const searchOption = document.getElementById("findOption");
-// const optionList = document.querySelectorAll(".option");
-// const searchDiv = document.querySelector(".search");
+} //getSelectValue
 
 
-// let subject = ['Java', 'PHP', 'Database', 'linux', 'javascript', 'Html, CSS']
-// // const subject = document.querySelector(".subject");
-// // console.log(subject);
 
-// searchOption.addEventListener("change", changeOption => {
-//     alert("changed");
-//     const searchBox1 = document.getElementById('searchBox');
-//     const searchBox = document.getElementById("searchBox").value.toLowerCase();
 
-//     searchBox1.addEventListener("keyup", searchSubject) 
-//     const tutorBox = document.querySelectorAll(".tutorBox");
-//     const subject = document.querySelectorAll(".subject");
-//     console.log(subject);
 
-//     for(var i=0; i<subject.length; i++) {
-//         let match = tutorBox[i].querySelector(".subject")[0]; 
-
-//         if(match) {
-//             let textValue = match.textContent || match.innerHTML
-            
-//             if(textValue.toLowerCase().includes(searchBox)) {
-//                 tutorBox[i].style.display="";
-//             } else {
-//                 tutorBox[i].style.display="none";
-//             }
-//         }
-//     }
-// });
 
 
 
