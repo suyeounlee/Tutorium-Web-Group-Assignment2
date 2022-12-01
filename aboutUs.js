01cd7e32d637d820680df59217896cbb4826f4b0
Suyeon Lee <lsy7674@gmail.com>
2022-12-01 14:43:24 -0500

    suyeon

---
+++ aboutUs.js
@@ -0,0 +1,13 @@
+
+const button = document.querySelector("#site")
+const site = document.querySelector(".flexbox-container-siteMap")
+
+button.addEventListener('click', ()=> {
+    if(site.style.display === 'none') {
+        site.style.display = 'flex';
+        button.value = 'Hide Sitemap'
+    } else{
+        site.style.display = 'none';
+        button.value = 'Show Sitemap'
+    }
+});  
