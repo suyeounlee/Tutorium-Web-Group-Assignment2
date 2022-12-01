ffbe4aca5f6488746ec59ad7259042c53df1346f
Suyeon Lee <lsy7674@gmail.com>
2022-12-01 15:08:32 -0500

    suyeon working

--- button.js
+++ button.js
@@ -7,8 +7,8 @@ function signup() {
     y.style.left = "50px";
     z.style.left = "110px";
 
-} 
- 
+}
+
 function login() {
     x.style.left = "50px";
     y.style.left = "450px";
--- signup.js
+++ signup.js
@@ -3,54 +3,62 @@ const username = document.getElementById('username');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
-const newsletter = document.getElementById('newsletter')
+
 
 form.addEventListener('submit', e => {
 	e.preventDefault();
 	
 	checkInputs();
 });
-  
+
 function checkInputs() {
 	// trim to remove the whitespaces
-	const usernameValue = username.value.trim();
 	const emailValue = email.value.trim();
 	const passwordValue = password.value.trim();
 	const password2Value = password2.value.trim();
+	const usernameValue = username.value.trim();
+
+	// if(usernameValue === '') {
+	// 	setErrorFor(username, 'Username should be non-empty.');
+	// } else if (usernameValue.length < 6) {
+	// 	setErrorFor(username, 'username should be at least 6 characters long');
+	// } else {
+	// 	setSuccessFor(username);
+	// 	return true;
+	// }
+
 	
-	if(usernameValue === ''|| usernameValue.length > 20 ) {
-		setErrorFor(username, 'Username should be non-empty and less than 20');
-	} else {
-		setSuccessFor(username);
-	}
+    // if(emailValue === '') {
+	// 	setErrorFor(email, 'Email cannot be blank');
+	// } else if (!isEmail(emailValue)) {
+	// 	setErrorFor(email, 'Not a valid email');
+	// } else {
+	// 	setSuccessFor(email);
+		
+	// }
 	
-    if(emailValue === '') {
-		setErrorFor(email, 'Email cannot be blank');
-	} else if (!isEmail(emailValue)) {
-		setErrorFor(email, 'Not a valid email');
-	} else {
-		setSuccessFor(email);
-	}
+	// if(passwordValue === ''){
+	// 	setErrorFor(password, 'Password cannot be blank');
+	// } 
+    // else if (passwordValue.length < 6) {
+	// 	setErrorFor(password, 'password should be at least 6 characters long');
+    // }  else if(( !(v = passwordValue.match(/[A-Z]/g)) || v.length < 1) || ( !(v = passwordValue.match(/[a-z]/g)) || v.length < 1))
+    //     { setErrorFor(password, 'password should have 1 uppercase, 1 lowercase');
+    // }else {
+	// 	setSuccessFor(password);
+	// 	return true;
+	// }
 	
-	if(passwordValue === ''){
-		setErrorFor(password, 'Password cannot be blank');
-	} 
-    else if (passwordValue.length < 6) {
-		setErrorFor(password, 'password should be at least 6 characters long');
-    }  else if(( !(v = passwordValue.match(/[A-Z]/g)) || v.length < 1) || ( !(v = passwordValue.match(/[a-z]/g)) || v.length < 1))
-        { setErrorFor(password, 'password should have 1 uppercase, 1 lowercase');
-    }else {
-		setSuccessFor(password);
-	}
+	// if(password2Value === '') {
+	// 	setErrorFor(password2, 'Password2 cannot be blank');
+	// } else if(passwordValue !== password2Value) {
+	// 	setErrorFor(password2, 'Passwords does not match');
+	// } else{
+	// 	setSuccessFor(password2);
+	// 	return true;
+	// }
 	
-	if(password2Value === '') {
-		setErrorFor(password2, 'Password2 cannot be blank');
-	} else if(passwordValue !== password2Value) {
-		setErrorFor(password2, 'Passwords does not match');
-	} else{
-		setSuccessFor(password2);
-	}
-}
+// }
 
 function setErrorFor(input, message) {
 	const formControl = input.parentElement;
@@ -66,4 +74,5 @@ function setSuccessFor(input) {
 	
 function isEmail(email) {
 	return /\S+@\S+\.\S+/.test(email);
+	return true;
 }
